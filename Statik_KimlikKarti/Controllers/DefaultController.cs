d30be739db3b262b08bdb750b2d948a1574ee98f
EDA.A <EDA.A@EDAWINDOWS>
2024-12-31 02:18:57 +0300

    Add project files.

---
+++ Statik_KimlikKarti/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Statik_KimlikKarti.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult StatikTema()
+        {
+            return View();
+        }
+    }
+}
---
+++ Statik_KimlikKarti/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Statik_KimlikKarti.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
